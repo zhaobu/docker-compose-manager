fb18cace63e9a98e045ffc2e1e2f79f321dcf26f
lw <zhaobulw@qq.com>
2019-09-16 19:40:56 +0800

    init

---
+++ nps/client/startclient.sh
@@ -0,0 +1,3 @@
+# nohup ./npc -server=120.79.123.194:9913 -vkey=ur1g7pkns8hljshs >/dev/null 2>&1 &
+nohup ./npc -server=47.52.39.39:2142 -vkey=1h79a8gxpubwdzfj >/dev/null 2>&1 &
+# ./npc -server=120.79.123.194:9913 -vkey=ur1g7pkns8hljshs
