13e1e96f72310a2efc344bc90b6f376cd0abbe03
lw <zhaobulw@qq.com>
2019-09-18 14:45:33 +0800

    update shell

--- nps/client/startclient.sh
+++ nps/client/startclient.sh
@@ -1,6 +1,6 @@
 # local test server
 # nohup ./npc -server=120.79.123.194:9913 -vkey=ur1g7pkns8hljshs >/dev/null 2>&1 &
-echo "执行的命令为 $*"
+echo "执行的命令为:$0 $*"
 case $1 in
 "cywin10")
     echo 'cywin10'
@@ -9,6 +9,10 @@ case $1 in
 "justhost")
     echo 'justhost'
     nohup ./npc -server=47.52.39.39:2142 -vkey=e12yfrv404v5zybq >./npc.log 2>&1 &
+    ;;
+*)
+    echo "Usage: ./startclient.sh cywin10"
+    exit 1
     ;;
 esac
 # test
