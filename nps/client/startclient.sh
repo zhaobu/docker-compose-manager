91832bc16cb018eddd2fef480400e0fcdc1231c4
lw <zhaobulw@qq.com>
2019-09-18 10:33:15 +0800

    1:添加cyhk

--- nps/client/startclient.sh
+++ nps/client/startclient.sh
@@ -1,3 +1,6 @@
+# local test server
 # nohup ./npc -server=120.79.123.194:9913 -vkey=ur1g7pkns8hljshs >/dev/null 2>&1 &
+# cywin10
 nohup ./npc -server=47.52.39.39:2142 -vkey=1h79a8gxpubwdzfj >/dev/null 2>&1 &
+# test
 # ./npc -server=120.79.123.194:9913 -vkey=ur1g7pkns8hljshs
