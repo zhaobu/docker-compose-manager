acca8a3242cd4fe7980f0bf9138fdecc1b5ae41e
lw <zhaobulw@qq.com>
2019-12-27 18:26:52 +0800

    add sonarqube

--- sonarqube/init_app.sh
+++ sonarqube/init_app.sh
@@ -1,2 +1,10 @@
-mkdir -p ./sonarqube/{data,logs,config,plugins}
-chown -R 2000:2000 ./sonarqube/{data,logs,config,plugins}
+# 执行 docker run --rm library/sonarqube:latest id sonarqube 查看uid，gid
+# 初始化sonarqube
+rm -rf ./sonarqube/{conf,data,logs,extensions}
+mkdir -p ./sonarqube/{conf,data,logs,extensions}
+chown -R 999:999 ./sonarqube/{conf,data,logs,extensions}
+
+# 初始化postgres
+rm -rf ./sonarqube_postgres
+mkdir -p ./sonarqube_postgres
+chown -R 70:70 ./sonarqube_postgres
