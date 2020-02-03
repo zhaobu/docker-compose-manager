17f7535a57663c205af0ceb186b46d168f62681a
remote work <zhaobulw@qq.com>
2020-02-03 20:44:51 +0800

    1:nps version 0.22.0

--- nps/client/winstartclient.bat
+++ nps/client/winstartclient.bat
@@ -1,4 +1,4 @@
 if "%1"=="hide" goto CmdBegin
 start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
 :CmdBegin
-npc.exe -server=47.52.39.39:2142 -vkey=19dw9xvcynyxfghm >> .\npc.log 2>&1
+npc.exe -server=120.79.123.194:55601 -vkey=y23gpqfb25s398iy -type=tcp >> .\npc.log 2>&1
